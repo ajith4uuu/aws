27bdb2bcfadeed9ed1cbba2581b27c774c5a0cf4
ajith4uuu <114105469+ajith4uuu@users.noreply.github.com>
2023-11-11 15:22:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of an existing EC2 KeyPair"
+  type        = string
+}
